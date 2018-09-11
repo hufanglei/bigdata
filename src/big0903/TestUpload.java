b106ca1d2ead68779222835b395eb9f5f598d525
hfl <690328661@qq.com>
2018-09-11 15:00:25 +0800

    hdfs javaAPI 文件上传2

--- src/big0903/TestUpload.java
+++ src/big0903/TestUpload.java
@@ -4,6 +4,7 @@ package big0903;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IOUtils;
 import org.junit.Test;
 
 import java.io.FileInputStream;
@@ -44,6 +45,28 @@ public class TestUpload {
         //关闭流
         input.close();
         output.close();
+    }
+
+    @Test
+    public void test2() throws Exception{
+        //指定当前的Hadoop的用户
+        System.setProperty("HADOOP_USER_NAME", "root");
+
+        //配置参数：指定NameNode地址
+        Configuration conf = new Configuration();
+        conf.set("fs.defaultFS", "hdfs://192.168.157.111:9000");
+
+        //创建一个客户端
+        FileSystem client = FileSystem.get(conf);
+
+        //构造一个输入流，从本地读入数据
+        InputStream input = new FileInputStream("d:\\temp\\hadoop-2.7.3.tar.gz");
+
+        //构造一个输出流 指向HDFS
+        OutputStream output = client.create(new Path("/folder111/b.tar.gz"));
+
+        //使用HDFS的一个工具类简化代码
+        IOUtils.copyBytes(input, output, 1024);
     }
 
 }
